718e27bbd4652b85aab9b52fa2ec7c949e7db0cf
Djorgy <djorgy2510@gmail.com>
2023-07-28 00:38:35 +0700

    initial commit

---
+++ gen-23.05-react-basic/src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Card = (props) => {
+  function getImgUrl(name) {
+    return new URL(`${name}`, import.meta.url).href;
+  }
+  return (
+    <div className="flex items-center justify-center w-full h-72 rounded-md px-4 py-4">
+      <div className="bg-white text-gray-700 w-56 min-h-[10rem] shadow-lg rounded-md overflow-hidden">
+        <img
+          className="w-full h-full object-cover"
+          src={getImgUrl(props.image)}
+          alt="image product"
+        />
+
+        <div className="flex p-5 flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <span className="px-3 py-1 rounded-full text-xs bg-gray-100">
+              stock ready
+            </span>
+            <span className="px-3 py-1 rounded-full text-xs bg-gray-100">
+              official store
+            </span>
+          </div>
+
+          <h2 className="font-semibold text-2xl overflow-ellipsis whitespace-nonwrap">
+            {props.name}
+          </h2>
+          <span className="text-xl font-bold">Rp.{props.price}</span>
+          <p>{props.desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- gen-23.05-react-basic/src/components/Navbar.jsx
+++ gen-23.05-react-basic/src/components/Navbar.jsx
@@ -2,17 +2,22 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full bg-green-400 flex flex-row content-center justify-between px-4 py-5 shadow ">
-      <div className="text-2xl">
+    <div className="w-full bg-green-400 flex flex-row content-center justify-between px-4 py-4 shadow font-montserrat">
+      <div className="text-2xl font-bold text-white">
         <a href="#">ShoppingKuy</a>
       </div>
-      <div className="flex flex-row items-center gap-4">
+      <div className="hidden md:flex flex-row items-center gap-4 text-white">
         <a href="#">Most Wanted</a>
-        <a href="#">Catagories</a>
-        <a href="#">Brands</a>
+        <a href="#">
+          Catagories<i class="fa fa-angle-down"></i>
+        </a>
+        <a href="#">
+          Brands
+          <i class="fa fa-angle-down"></i>
+        </a>
         <a href="#">New Arrivals</a>
       </div>
-      <div className="flex flex-row items-center gap-4">
+      <div className="hidden md:flex flex-row items-center gap-4">
         <form>
           <input
             type="text"
@@ -26,8 +31,17 @@ const Navbar = () => {
             <i class="fa fa-search"></i>
           </button>
         </form>
-        <a href="#">Login</a>
-        <a href="#">Cart</a>
+        <a href="#" className="text-white">
+          <i class="fa fa-user"></i>
+        </a>
+        <a href="#" className="text-white">
+          <i class="fa fa-shopping-bag"></i>
+        </a>
+      </div>
+      <div className="block text-white font-bold md:hidden">
+        <a href="#">
+          <i class="fa fa-bars"></i>
+        </a>
       </div>
     </div>
   );
